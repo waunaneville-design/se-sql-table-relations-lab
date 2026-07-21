9a2089cb22f5819562f63fb42db6b439bc9dcb34
Lascelles <waunaneville@gmail.com>
2026-07-21 08:42:27 +0300

    Query Boston employees with names and job titles
    Added SQL join between employees and offices to return first and last names of staff located in the Boston office.

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ conn = sqlite3.connect('data.sqlite')
 
 pd.read_sql("""SELECT * FROM sqlite_master""", conn)
 
+# STEP 1
+# Return the first and last names and job titles for all employees in Boston
+df_boston = pd.read_sql("""
+SELECT e.firstName, e.lastName
+FROM employees e
+JOIN offices o ON e.officeCode = o.officeCode
+WHERE o.city = 'Boston'
+""", conn)
